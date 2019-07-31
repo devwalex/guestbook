a166d5b83a42cfb5246811cf97d8f104c357d42f
Salami Usman Olawale <salamiusmanolawale@gmail.com>
2019-07-31 05:52:45 +0100

    Add express-handlebars

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
 const app = express();
 
+// Connect to Mongoose
+mongoose.connect('mongodb://localhost:27017/guestbook', {
+    useNewUrlParser: true
+}).then(() => {
+    console.log('MongoDB connected.....');
+}).catch(err => console.log(err));
+
+// Handlebars MiddleWare
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is listening to port: ${port}`);
-})
+});
