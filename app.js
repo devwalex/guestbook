04b2b59b60f2c4f1c88071865e6c707509ae17ed
Salami Usman Olawale <salamiusmanolawale@gmail.com>
2019-08-02 17:36:54 +0100

    modify error on the Index Route

--- app.js
+++ app.js
@@ -8,116 +8,121 @@ const session = require('express-session');
 const app = express();
 
 // Connect to Mongoose
-mongoose.connect('mongodb://localhost:27017/guestbook', {
-    useNewUrlParser: true
-}).then(() => {
-    console.log('MongoDB connected.....');
-}).catch(err => console.log(err));
+mongoose
+	.connect('mongodb://localhost:27017/guestbook', {
+		useNewUrlParser: true
+	})
+	.then(() => {
+		console.log('MongoDB connected.....');
+	})
+	.catch((err) => console.log(err));
 
 // Handlebars MiddleWare
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
+app.engine(
+	'handlebars',
+	exphbs({
+		defaultLayout: 'main'
+	})
+);
 app.set('view engine', 'handlebars');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // BodyParser MiddlwWare
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+);
 app.use(bodyParser.json());
 
 // Express Session Middleware
-app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-}));
+app.use(
+	session({
+		secret: 'secret',
+		resave: true,
+		saveUninitialized: true
+	})
+);
 
-// Flash Middleware 
+// Flash Middleware
 app.use(flash());
 
 // Load Guest Model
 require('./models/Guest');
-const Guest = mongoose.model('guest')
+const Guest = mongoose.model('guest');
 
 // Global Variables
 app.use((req, res, next) => {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    next();
-})
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	next();
+});
 
 // Index Route --View
 app.get('/', (req, res) => {
-    res.render('index');
+	res.render('index');
 });
 
 // Index Router -- Post
 app.post('/', (req, res) => {
-    let errors = [];
-    if (!req.body.fullname || typeof (req.body.fullname.trim()) !== 'undefined') {
-        errors.push({
-            text: 'Please enter your name'
-        });
-    }
-    if (!req.body.email || typeof (req.body.fullname.trim()) !== 'undefined') {
-        errors.push({
-            text: 'Please enter your email'
-        });
-    }
-    if (!req.body.comment || typeof (req.body.fullname.trim()) !== 'undefined') {
-        errors.push({
-            text: 'Please enter a comment'
-        })
-    }
-    if (errors.length > 0) {
-        res.render('index', {
-            errors: errors,
-            fullname: req.body.fullname,
-            email: req.body.email,
-            comment: req.body.comment
-        })
-    } else {
-        const newGuest = {
-            fullname: req.body.fullname,
-            email: req.body.email,
-            comment: req.body.comment
-        }
-        new Guest(newGuest)
-            .save()
-            .then((guest) => {
-                req.flash('success_msg', 'Guest Added Sucessfully')
-                res.redirect('all-guest')
-                console.log('saved', guest);
-            })
-    }
-
+	let errors = [];
+	if (!req.body.fullname || req.body.fullname.trim().length === 0) {
+		errors.push({
+			text: 'Please enter your name'
+		});
+	}
+	if (!req.body.email || req.body.email.trim().length === 0) {
+		errors.push({
+			text: 'Please enter your email'
+		});
+	}
+	if (!req.body.comment || req.body.comment.trim().length === 0) {
+		errors.push({
+			text: 'Please enter a comment'
+		});
+	}
+	if (errors.length > 0) {
+		res.render('index', {
+			errors: errors,
+			fullname: req.body.fullname,
+			email: req.body.email,
+			comment: req.body.comment
+		});
+	} else {
+		const newGuest = {
+			fullname: req.body.fullname,
+			email: req.body.email,
+			comment: req.body.comment
+		};
+		new Guest(newGuest).save().then((guest) => {
+			req.flash('success_msg', 'Guest Added Sucessfully');
+			res.redirect('all-guest');
+			console.log('saved', guest);
+		});
+	}
 });
 
 // Edit Route -- View
 app.get('/edit', (req, res) => {
-    res.render('edit');
+	res.render('edit');
 });
 
 // All Guest Route -- View
 app.get('/all-guest', (req, res) => {
-    Guest.find()
-        .sort({
-            date: 'desc'
-        })
-        .then((guest) => {
-            res.render('all-guest', {
-                guest
-            });
-        })
+	Guest.find()
+		.sort({
+			date: 'desc'
+		})
+		.then((guest) => {
+			res.render('all-guest', {
+				guest
+			});
+		});
 });
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is listening to port: ${port}`);
+	console.log(`Server is listening to port: ${port}`);
 });
